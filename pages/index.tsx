ca4486a76cae0c33b805a73710a368a5ed809e6a
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2022-12-28 16:07:15 +0400

    ci/cd: remove ci cd pipeline

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,12 @@ const Home: NextPage = () => {
           alt='landing image'
         />
       </div>
-
-
-
-
-
-
-
-
-
-      
       <div className='flex-grow flex flex-col items-start justify-center space-y-16 bg-orange-600 pl-20'>
         <h1 className='text-white text-8xl leading-[7rem] font-extrabold capitalize'>
           chess says{' '}
           <span className='text-black text-base font-normal'>a lot about</span>{' '}
           <br />
-          who you are but not really :)
+          who you are
         </h1>
         <BlackLinkWithArrowRight
           link='/personal-information'
